f2ea6f2195fb119a1c474f4ad5ee86815a44a7ec
ADEEL3335 <adeel.bsse3335@iiu.edu.pk>
2019-12-26 17:17:11 +0500

    adding firebase

--- src/app/pages/singup/singup.page.ts
+++ src/app/pages/singup/singup.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { auth } from 'firebase/app';
+//import { AngularFirestore } from '@angular/fire/firebase';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-singup',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./singup.page.scss'],
 })
 export class SingupPage implements OnInit {
+  username: string = ""
+	password: string = ""
+	cpassword: string = ""
 
-  constructor() { }
+
+  constructor(public afAuth: AngularFireAuth,
+		//public afstore: AngularFirestore,
+		public user: UserService,
+		public alertController: AlertController,
+		public router: Router) { }
 
   ngOnInit() {
   }
+  async presentAlert(title: string, content: string) {
+		const alert = await this.alertController.create({
+			header: title,
+			message: content,
+			buttons: ['OK']
+		})
+
+		await alert.present()
+  }
+  async register() {
+		const { username, password, cpassword } = this
+		if(password !== cpassword) {
+			return console.error("Passwords don't match")
+		}
+
+		try {
+			const res = await this.afAuth.auth.createUserWithEmailAndPassword(username + '@codedamn.com', password)
+
+			this.afstore.doc(`users/${res.user.uid}`).set({
+				username,
+				password
+			})
+
+			this.user.setUser({
+				username,
+				uid: res.user.uid
+			})
+
+			this.presentAlert('Success', 'You are registered!')
+			this.router.navigate(['/login'])
+
+		} catch(error) {
+			console.dir(error)
+		}
+	}
+
 
 }
